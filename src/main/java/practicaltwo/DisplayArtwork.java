d540aae96b09252a2a06135da198ea3893385833
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-09-06 16:25:50 -0400

    Starter files for practical 2

---
+++ src/main/java/practicaltwo/DisplayArtwork.java
@@ -0,0 +1,35 @@
+package practicaltwo;
+
+import java.util.Date;
+import java.util.Scanner;
+
+/** This class has a template that displays ASCII artwork.
+ * The first line displays the name of the programmer and the date. Then, the
+ * program's main method displays nine lines of artwork that use characters. The
+ * program should produce the ten total lines of output by using only a total of
+ * nine println statements.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing source code
+ *
+ * @author Janyl Jumadinova
+ */
+
+public class DisplayArtwork {
+
+  /** The main entry point for DisplayArtwork.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window. The println statements make the program display artwork
+   *  in the output, following the aforementioned requirements.
+   *
+   * @param args The command-line arguments
+   **/
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Janyl Jumadinova " + new Date());
+    // display the lines of the artwork
+    // while only using a total of ten println statements
+    // in the entire main method (including the one above)
+  }
+
+}
